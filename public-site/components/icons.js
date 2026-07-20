d30b08e4a2ee3db5144f09a9ac72a75cddf7e4ea
CoffeeOneUA <coffeeone.chatbot@ukr.net>
2026-07-20 15:20:48 +0300

    Update icons.js

--- public-site/components/icons.js
+++ public-site/components/icons.js
@@ -47,6 +47,6 @@ export const SERVICE_ICONS = {
 
 export const CHECK_ICON = (
   <svg viewBox="0 0 24 24" fill="none">
-    <path d="M5 13l4 4L19 7" stroke="currentColor" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5 13l4 4L19 7" stroke="#FFFFFF" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
